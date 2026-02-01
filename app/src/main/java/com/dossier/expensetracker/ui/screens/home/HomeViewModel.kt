7086dfd2844141bdf3cbe7a8090fc561e302b6bb
narendranath.maddikeri2007@gmail.com <narendranath.maddikeri2007@gmail.com>
2026-02-01 17:26:45 +0530

    added code for HomeViewModel.kt

--- app/src/main/java/com/dossier/expensetracker/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/dossier/expensetracker/ui/screens/home/HomeViewModel.kt
@@ -2,8 +2,7 @@ package com.dossier.expensetracker.ui.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.chozhanaadu.androidtemplate.domain.usecase.GetItemsUseCase
-import com.chozhanaadu.androidtemplate.domain.usecase.RefreshItemsUseCase
+import com.dossier.expensetracker.domain.usecase.GetExpenseUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,4 +11,31 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getExpenseUseCase: GetExpenseUseCase
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+    init {
+        observeItems()
+    }
+
+    private fun observeItems(){
+        viewModelScope.launch {
+            getExpenseUseCase()
+                .catch { exception ->
+                    _uiState.update { it.copy(error = exception.message) }
+                }
+                .collect { items ->
+                    _uiState.update { it.copy(
+                        items = items,
+                        error = null
+                    ) }
+                }
+        }
+    }
+}
 
