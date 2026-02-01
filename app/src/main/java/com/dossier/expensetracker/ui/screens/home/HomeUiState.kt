e9b77804ec398c594694ca436cae694bb6436936
narendranath.maddikeri2007@gmail.com <narendranath.maddikeri2007@gmail.com>
2026-02-01 17:26:32 +0530

    added code for HomeUiState.kt

--- app/src/main/java/com/dossier/expensetracker/ui/screens/home/HomeUiState.kt
+++ app/src/main/java/com/dossier/expensetracker/ui/screens/home/HomeUiState.kt
@@ -1,2 +1,10 @@
 package com.dossier.expensetracker.ui.screens.home
 
+import com.dossier.expensetracker.domain.model.Expense
+
+
+data class HomeUiState(
+    val items: List<Expense> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
