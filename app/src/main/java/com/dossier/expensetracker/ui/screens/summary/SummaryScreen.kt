eea3d62970b72f47460de6582e048849fa04dcc3
narendranath.maddikeri2007@gmail.com <narendranath.maddikeri2007@gmail.com>
2026-02-03 15:20:47 +0530

    added SummaryScreen.kt

---
+++ app/src/main/java/com/dossier/expensetracker/ui/screens/summary/SummaryScreen.kt
@@ -0,0 +1,12 @@
+package com.dossier.expensetracker.ui.screens.summary
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.dossier.expensetracker.ui.screens.home.HomeViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SummaryScreen(viewModel: HomeViewModel = hiltViewModel()) {
+
+}
